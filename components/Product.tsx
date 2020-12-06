6115242260d8729922c494bb25922bf8016a0caa
mtauqeer248 <62745971+mtauqeer248@users.noreply.github.com>
2020-12-06 15:59:58 +0500

    netlify deploy

--- components/Product.tsx
+++ components/Product.tsx
@@ -2,8 +2,9 @@ import React from "react"
 import { Theme, makeStyles, createStyles } from "@material-ui/core/styles"
 import ButtonBase from "@material-ui/core/ButtonBase"
 import Typography from "@material-ui/core/Typography"
-
+import { useSelector } from "react-redux"
 import { ProductItem } from "../global"
+import { add, store } from "../store"
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -83,7 +84,8 @@ const useStyles = makeStyles((theme: Theme) =>
 const Product = () => {
   const classes = useStyles({})
 
-  const products = [] // TODO
+  //const products = [] // TODO
+  const products = useSelector((state: ProductItem[]) => state)
 
   return (
     <div className={classes.root}>
@@ -94,9 +96,10 @@ const Product = () => {
           className={classes.image}
           focusVisibleClassName={classes.focusVisible}
           disabled={product.added}
-          onClick={() => {
+        //  onClick={() => {
             /* Add to basket */
-          }}
+        //  }}
+          onClick={() => store.dispatch(add(product))}
           style={{
             width: `${100 / products.length}%`
           }}
---
+++ store/index.ts
@@ -0,0 +1,43 @@
+
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+import { INITIAL_STATE } from './state'
+
+const basketSlice = createSlice({
+  name: 'basket',
+  initialState: INITIAL_STATE,
+  reducers: {
+    add: (state, action) => {
+        return state.map(item => {
+                   if (item.id !== action.payload.id) {
+                     return item
+                  }
+        
+                  return {
+                    ...item,
+                    added: true
+                  }
+                })
+      // add item to basket using `state` and `action` props
+   // 
+      },
+      remove: (state, action) => {
+     return state.map(item => {
+          if (item.id !== action.payload.id) {
+           return item
+      }
+
+         return {
+         ...item,
+            added: false      }
+          
+        })
+      }
+    }
+ })
+
+
+const store = configureStore({ reducer: basketSlice.reducer })
+
+export const { add,remove } = basketSlice.actions
+
+export { basketSlice, store }
